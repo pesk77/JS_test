18e49d66f75dc5bfe4cb090da27e158db8b6d49f
pesk77 <pesk@email.cz>
2017-12-12 10:01:28 +0100

    Test objektu

--- Spravce_ukolu.js
+++ Spravce_ukolu.js
@@ -1,3 +1,13 @@
 var ukoly;
 
 ukoly = ["Zaplatit účet za telefon.", "Npasat nejprodávanější román.", "Jít na procházku se psem."];
+
+var klic, data;
+data = {
+    krestniJmeno: "James",
+    prijmeni: "Kirk",
+    povolani: "kapitán"
+}
+for (klic in data) {
+    alert(klic + " je " + data[klic]);
+}
